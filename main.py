1e4c85bc0c781c69a6dd79cebef01a0227153a96
Donovan <donnie58744@gmail.com>
2022-04-19 21:19:35 -0700

    Working
    Messages are now laid out in a grid fashion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QVBoxLayout, QWidget
+from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QTextEdit
 from PyQt6 import uic
+from PyQt6.QtCore import Qt
 import sys
 import os
 import requests
@@ -10,6 +11,24 @@ subprocess.check_call([sys.executable, "-m", "pip", "install", "--upgrade", "pip
 subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "req.txt"])
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
+
+class working():
+    username = ''
+    password = ''
+    messageCount = 0
+    maxMessages= 21
+    maxOneRow = 7
+
+    def sendServerRequest(place, request, username, password):
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:55.0) Gecko/20100101 Firefox/55.0',
+        }
+
+        pload = {'request':request, 'user':username, 'pass':password}
+        r = requests.post('https://www.quackyos.com/QuackShack/' + place, data=pload, headers=headers)
+        output = str(r.text)
+        return output
+
 class messagesPage(QWidget):
     """
     This "window" is a QWidget. If it has no parent, it
@@ -17,11 +36,44 @@ class messagesPage(QWidget):
     """
     def __init__(self):
         super().__init__()
+        
         uic.loadUi(dir_path+'/frames/messagePage.ui', self)
+
+        # Split messages up into sperate objects
+        messages = working.sendServerRequest('backend/getMessages.php','getMessages', working.username, working.password).split("<br>")
+        labelPlaceColum = -1
+        row = 0
+        layoutCheck = 0
+        
+        # Math bullshit
+        # Bassically loop however many times there are messages
+        for i in messages:
+            # This is to keep track of current amount of messages
+            working.messageCount += 1
+            # Only allow x amount of messages to be displayed
+            if (working.messageCount <= working.maxMessages):
+                # Allow 4 messages in one row
+                if (layoutCheck == working.maxOneRow):
+                    # Change row
+                    row += 1
+                    # reset vars
+                    layoutCheck = 0
+                    labelPlaceColum = -1
+                layoutCheck += 1
+                # Increase colum pos
+                labelPlaceColum += 1
+                # Create messages and set text
+                messageArea = QTextEdit()
+                messageArea.setText(i)
+                messageArea.setReadOnly(True)
+
+                # Add messages to grid
+                self.gridLayout.addWidget(messageArea, row, labelPlaceColum)
+
+        self.showFullScreen()
 
 
 class MainWindow(QMainWindow):
-
     def __init__(self):
         super().__init__()
         self.w = None  # No external window yet.
@@ -33,20 +85,12 @@ class MainWindow(QMainWindow):
             self.w = window()
             self.w.show()
 
-    def sendServerRequest(self, place, request):
-        headers = {
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:55.0) Gecko/20100101 Firefox/55.0',
-        }
-
-        pload = {'request':request, 'user':self.usernameInput.text(), 'pass':self.passwordInput.text()}
-        r = requests.post('https://www.quackyos.com/QuackShack/' + place, data=pload, headers=headers)
-        output = str(r.text)
-        return output
-
     def login(self):
         if (self.usernameInput.text() != '' and self.passwordInput.text() != ''):
-            if (self.sendServerRequest('backend/login.php', '') == "Logged In"):
+            if (working.sendServerRequest('backend/login.php', '', self.usernameInput.text(), self.passwordInput.text()) == "Logged In"):
                 print('Change Page')
+                working.username = self.usernameInput.text()
+                working.password = self.passwordInput.text()
                 self.hide()
                 self.openWindow(messagesPage)
             else:
