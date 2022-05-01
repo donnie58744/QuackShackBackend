6378f4b173097c83de2f9a1af2431c0e74b03078
Donovan <donnie58744@gmail.com>
2022-05-01 14:10:44 -0700

    Changes
    Messages are checked and created using threads

--- main.py
+++ main.py
@@ -13,9 +13,9 @@ else:
     from audioplayer import AudioPlayer
 
 from threading import Thread
-from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QTextEdit
+from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QTextEdit, QPushButton
 from PyQt6 import uic
-from PyQt6.QtCore import QTimer
+from PyQt6.QtCore import QThread, pyqtSlot, QObject, pyqtSignal, Qt
 import os
 import requests
 from time import sleep
@@ -25,8 +25,9 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 # Main functions go here
 class working():
     # GUI Controls
-    maxMessages= 12
     maxOneRow = 4
+    maxOneColum = 3
+    maxMessages= maxOneRow*maxOneColum
     messagesMaxHeight = 400
     messagesMaxWidth = 400
 
@@ -57,6 +58,69 @@ class working():
         output = str(r.text)
         return output
 
+class checkMessagesThreaded(QObject):
+    def __init__(self, signal_to_emit, parent=None):
+        super().__init__(parent)
+        self.signal_to_emit = signal_to_emit
+
+    @pyqtSlot()
+    def executeThread( self ):
+        while True:
+            print('Slot doing stuff in:', QThread.currentThread())
+            sleep(5)
+
+            try:
+                # Split messages up into sperate objects
+                messages = working.sendServerRequest('backend/getMessages.php','getMessages', working.username, working.password, working.maxMessages).split(working.charSplit)
+                
+                # Math bullshit
+                # Bassically loop however many times there are messages
+                for i in messages:
+                    # check if there is anything in DB
+                    if (len(i) != 0):
+                        # Gets current post it ID
+                        working.idCounter = i.split('|')[0]
+                        working.messageFunction = i.split('|')[3]
+                        if (working.messageCountTemp == 0):
+                            working.idCurrent = working.idCounter
+
+                        messagesLeft = int(working.idCurrent) - int(working.idTemp)
+                        print(messagesLeft)
+                        if (messagesLeft != 0):
+                            # If new ID is added then execute message stuff
+                            while working.queueCounter < int(working.idCurrent) - int(working.idTemp) and working.messageCountLock:
+                                working.messageQueue.insert(0, working.messageFunction)
+                                
+                                self.signal_to_emit.emit(str(i))
+                                working.queueCounter += 1
+                                break
+                                
+                        working.messageCountTemp += 1
+
+                # Check if theres any messages
+                if (working.messageCountLock):
+                    # Prevent range error
+                    if (working.idTemp == "" or working.idCurrent == ""):
+                        working.idTemp = 0
+                        working.idCurrent = 0
+                    # This is a queue for the sound effect
+                    for x in range(0, len(working.messageQueue)):
+                        # Alert if new message comes in
+                        audioThread= Thread(target=audioPlayer.playSound(working.messageQueue[x]))
+                        audioThread.start()
+                    working.messageQueue = []
+
+                # Reset Stuff
+                working.messageCountLock = True
+                working.messageCountTemp = 0
+                working.queueCounter = 0
+
+                # Sets the temp id last so it can check the current id first
+                if (working.messageCountTemp == 0):
+                    working.idTemp = working.idCurrent
+            except Exception as e:
+                print("Create Message Error: " + str(e))
+
 class audioPlayer():
     def playSound(sound):
         try:
@@ -73,96 +137,65 @@ class messagesPage(QWidget):
     This "window" is a QWidget. If it has no parent, it
     will appear as a free-floating window as we want.
     """
+    sig = pyqtSignal(str)
     def __init__(self):
         super().__init__()
         
         uic.loadUi(dir_path+'/res/frames/messagePage.ui', self)
 
-        # Setup updater
-        self.updateMessages=QTimer()
-        self.updateMessages.timeout.connect(self.createMessages)
-
-        # Start updating messages
-        self.updateMessages.start(5000)
-
         self.showFullScreen()
+
+        # Start thread button
+        self.startMessageThreadButton = QPushButton()
+        self.startMessageThreadButton.setStyleSheet("QPushButton"
+                             "{"
+                             "background-color : lightgreen;"
+                             "}"
+                             "QPushButton::pressed"
+                             "{"
+                             "background-color : red;"
+                             "}")
+        self.gridLayout.addWidget(self.startMessageThreadButton)
         
+        self.colum = -1
+        self.row = 0
+        self.layoutCheck = 0
 
-    def createMessages(self):
-        try:
-            # Split messages up into sperate objects
-            messages = working.sendServerRequest('backend/getMessages.php','getMessages', working.username, working.password, working.maxMessages).split(working.charSplit)
-            colum = -1
-            row = 0
-            layoutCheck = 0
+        self.worker = checkMessagesThreaded(self.sig)
+        thread = QThread(self) 
+        self.worker.moveToThread(thread)
+        thread.start()
+        self.startMessageThreadButton.clicked.connect(self.worker.executeThread)
+        self.sig.connect(self.createMessages)
+
+    @pyqtSlot(str)
+    def createMessages(self, text):
+        print('Creating Messages')
+
+        # Allow x amount of messages in one row
+        if (self.layoutCheck == working.maxOneRow):
+            # Change row
+            self.row += 1
             
-            # Math bullshit
-            # Bassically loop however many times there are messages
-            for i in messages:
-                # check if there is anything in DB
-                if (len(i) != 0):
-                    # Gets current post it ID
-                    working.idCounter = i.split('|')[0]
-                    working.messageFunction = i.split('|')[3]
-                    if (working.messageCountTemp == 0):
-                        working.idCurrent = working.idCounter
-                    
-                    # If new ID is added then execute message stuff
-                    if (working.idCurrent != working.idTemp):
-                        while working.queueCounter < int(working.idCurrent) - int(working.idTemp) and working.messageCountLock:
-                            working.messageQueue.insert(0, working.messageFunction)
-                            working.queueCounter += 1
-                            break
-                        #--------Create Messages--------
-                        # Allow x amount of messages in one row
-                        if (layoutCheck == working.maxOneRow):
-                            # Change row
-                            row += 1
-                            # reset vars
-                            layoutCheck = 0
-                            colum = -1
-                        layoutCheck += 1
-                        # Increase colum pos
-                        colum += 1
-                        # Create messages and set props
-                        messageArea = QTextEdit()
-                        messageArea.setText(i)
-                        messageArea.setReadOnly(True)
-                        messageArea.setStyleSheet('background-color: rgb(209, 183, 15); padding-top: 10px; border-top: 50px solid; border-top-color: rgb(158, 158, 158);')
-                        messageArea.setMaximumHeight(working.messagesMaxHeight)
-                        messageArea.setMaximumWidth(working.messagesMaxWidth)
-
-                        # Add messages to grid
-                        self.gridLayout.addWidget(messageArea, row, colum)
-
-                        #------------------------------
-
-                            
-                    working.messageCountTemp += 1
-
-            # Check if theres any messages
-            if (working.messageCountLock):
-                # Prevent range error
-                if (working.idTemp == "" or working.idCurrent == ""):
-                    working.idTemp = 0
-                    working.idCurrent = 0
-                # This is a queue for the sound effect
-                for x in range(0, len(working.messageQueue)):
-                    # Alert if new message comes in
-                    audioThread= Thread(target=audioPlayer.playSound(working.messageQueue[x]))
-                    audioThread.start()
-                working.messageQueue = []
-
-            # Reset Stuff
-            working.messageCountLock = True
-            working.messageCountTemp = 0
-            working.queueCounter = 0
-
-            # Sets the temp id last so it can check the current id first
-            if (working.messageCountTemp == 0):
-                working.idTemp = working.idCurrent
-        except Exception as e:
-            print("Create Message Error: " + str(e))
+            # reset vars
+            self.layoutCheck = 0
+            self.colum = -1
+        if (self.row == working.maxOneColum):
+            # reset vars
+            self.layoutCheck = 0
+            self.row = 0
+        self.layoutCheck += 1
+        # Increase colum pos
+        self.colum += 1
+        # Create messages and set props
+        self.messageArea = QTextEdit()
+        self.messageArea.setReadOnly(True)
+        self.messageArea.setStyleSheet('background-color: rgb(209, 183, 15); padding-top: 10px; border-top: 50px solid; border-top-color: rgb(158, 158, 158);')
+        self.messageArea.setMaximumHeight(working.messagesMaxHeight)
+        self.messageArea.setMaximumWidth(working.messagesMaxWidth)
+        # Add messages to grid
+        self.messageArea.setText(text)
+        self.gridLayout.addWidget(self.messageArea, self.row, self.colum)
         
 
 class MainWindow(QMainWindow):
